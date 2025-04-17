8c87c3a3dec7526945fc9831b28cc552f2a3c0b2
sejin <63094026+tpwls9494@users.noreply.github.com>
2025-04-17 16:03:52 +0900

    Update urls.py

--- django_ec2/django_ec2/urls.py
+++ django_ec2/django_ec2/urls.py
@@ -19,9 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< Updated upstream
-    path('main/', include('main.urls'))
-=======
     path('main/', include('main.urls')),
->>>>>>> Stashed changes
 ]
