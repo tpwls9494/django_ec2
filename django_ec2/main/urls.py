f9b8fc1b33d4b72c2c72c8d0fdc8284295c1c5d3
sejin <63094026+tpwls9494@users.noreply.github.com>
2025-04-17 16:04:22 +0900

    Update urls.py

--- django_ec2/main/urls.py
+++ django_ec2/main/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from .views import hello_view
 
 urlpatterns = [
-<<<<<<< Updated upstream
-    path('', hello_view)
-=======
     path('hello/', hello_view, name='hello_view'),
->>>>>>> Stashed changes
 ]
