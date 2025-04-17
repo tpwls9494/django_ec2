2cbd2fa85acdd54516ccaac785d012b3bc6c5e31
sejin <63094026+tpwls9494@users.noreply.github.com>
2025-04-17 15:59:04 +0900

    Update settings.py

--- django_ec2/django_ec2/settings.py
+++ django_ec2/django_ec2/settings.py
@@ -123,10 +123,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
